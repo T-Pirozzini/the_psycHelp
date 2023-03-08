f85cd2ae6be880d0d12709ead81bd7b24e0c7302
Richard Au <au.richard4@gmail.com>
2023-03-08 15:50:59 -0800

    Fixing firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:the_psychelp/pages/landing_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ThePsycHelp());
 }
 
